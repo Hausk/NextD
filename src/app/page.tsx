3d5a4442df9a5441ca9026a9ac05315cc8219c8c
Hausk <dbessa.jonathan@gmail.com>
2023-12-22 10:31:22 +0100

    add: first template example and test for framer.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,48 @@
-import Image from 'next/image'
+'use client'
+import { useEffect, useState, useRef, RefObject } from "react";
+import Mouse from "./components/Mouse"
+import { motion } from 'framer-motion'
+
+function useFollowPointer(ref: RefObject<HTMLElement>) {
+  const [point, setPoint] = useState({ x: 0, y: 0 });
+
+  useEffect(() => {
+    if (!ref.current) return;
+
+    const handlePointerMove = ({ clientX, clientY }: MouseEvent) => {
+      const element = ref.current!;
+
+      const x = clientX - element.offsetLeft - element.offsetWidth / 2;
+      const y = clientY - element.offsetTop - element.offsetHeight / 2;
+      setPoint({ x, y });
+    };
+
+    window.addEventListener("pointermove", handlePointerMove);
+
+    return () => window.removeEventListener("pointermove", handlePointerMove);
+  }, []);
+
+  return point;
+}
 
 export default function Home() {
+
+  const [point, setPoint] = useState({ x: 0, y: 0 });
+  const ref = useRef(null);
+  const { x, y } = useFollowPointer(ref);
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Homepage</h1>
-    </main>
+    <>
+    <motion.div
+      ref={ref}
+      className="box w-[150px] h-[150px] rounded-full bg-red-500"
+      animate={{ x, y }}
+      transition={{
+        type: "spring",
+        damping: 3,
+        stiffness: 50,
+        restDelta: 0.001
+      }}
+    />
+    </>
   )
 }
---
+++ src/app/works/page.tsx
@@ -0,0 +1,91 @@
+'use client'
+import Image from 'next/image'
+import { Swiper, SwiperSlide } from "swiper/react"
+import { Parallax } from "swiper/modules";
+import { useState } from 'react';
+const elements = Array.from({length: 10}, (_, index) => {
+  <Image key={index} src={'https://picsum.photos/200/300?random=' + index} width={100} height={100} alt='random'/>
+})
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+import 'swiper/css';
+
+export default function Page() {
+  const imgUrls = numbers.map((_, index) => `https://picsum.photos/200/300?random=${index}`);
+  const [currentSlide, setCurrentSlide] = useState(0);
+  const [thumbsSwiper, setThumbsSwiper] = useState(null);
+  const slideVariants = {
+      hidden: { x: "100%" },
+      visible: { x: 0 },
+  };
+
+  const img1 = 'https://fastly.picsum.photos/id/610/400/600.jpg?hmac=lBzINTZQOsnRP3X3_L5t3gvPWyj9z8tLL4IkvSn48x0'
+  const img2 = 'https://fastly.picsum.photos/id/694/400/600.jpg?hmac=lKkl78L9Lb2Mw4DJ2f-Yw4gyYt1cbQObNszqiAJto4E'
+  const img3 = 'https://fastly.picsum.photos/id/167/400/600.jpg?hmac=IaHo-3Pn-iSsjtRY7DCyw5VvorKVSSERQSgdD7T1xHw'
+  const img4 = 'https://fastly.picsum.photos/id/550/400/600.jpg?hmac=BVOnFTLer0NJBfa6eszNm4RLeEfWPVcAQ7GOrGTqT7w'
+  const img5 = 'https://fastly.picsum.photos/id/119/400/600.jpg?hmac=CQUOkA4wYD1HbPxsyd254_MO7AG8M5I1xFtr404s-SY'
+
+  const [lists, setLists] = useState([
+      { title: "Lisbonne", year: "2023", img: img1, quantity: 50 },
+      { title: "Portugal", year: "2023", img: img2, quantity: 10 },
+      { title: "Italie", year: "2022", img: img3, quantity: 22 },
+      { title: "France", year: "2022", img: img4, quantity: 41 },
+      { title: "Porto", year: "2021", img: img5, quantity: 105 },
+  ]);
+    const handleSlideChange = (swiper: any) => {
+      setCurrentSlide(swiper.activeIndex);
+  };
+  return (
+    <>
+      <div className="h-screen overflow-hidden">
+        <Swiper
+          onSlideChange={handleSlideChange}
+          pagination={{ dynamicBullets: true }}
+          modules={[Parallax]}
+          spaceBetween={0}
+          centeredSlides={true}
+          slidesPerView={1}
+          loop={true}
+          parallax={true}
+          className="h-screen z-100 w-screen m-auto"
+          breakpoints={{
+            320: {
+                slidesPerView: 1,
+                direction: 'vertical'
+            },
+            640: {
+                slidesPerView: 2,
+                spaceBetween: 50,
+                direction: 'horizontal',
+            }
+        }}
+        >
+          {lists.map((item, index) => (
+            <SwiperSlide key={index} className="w-full flex relative">
+              <div className="m-auto w-[80%] md:w-[60%] relative">
+                <Image
+                  className="w-full m-auto pt-5 rounded-sm"
+                  src={item.img}
+                  alt="slide"
+                  width={400}
+                  height={600}
+                  data-swiper-parallax="0"
+                />
+                <div className="-rotate-12 absolute top-0 -left-[1rem] mix-blend-difference">
+                  <p className="text-center font-medium text-5xl text-white rounded-sm" data-swiper-parallax={window.innerWidth > window.innerHeight ? "-200%" : "-500%"}>
+                      {item.title}
+                  </p>
+                </div>
+                <div className="-rotate-6 absolute bottom-0 -right-[1rem] mix-blend-difference">
+                  <p className="text-center font-medium text-5xl text-white" data-swiper-parallax={window.innerWidth > window.innerHeight ? "-200%" : "-500%"}>
+                      {item.year}
+                  </p>
+                  </div>
+            </div>
+          </SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
+    </>
+  )
+}
